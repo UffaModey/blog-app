6d65d169e4f0158bffb37083d511fa63aad60d15
Fafa Modey <fafa.modey@hedgehoglab.com>
2022-01-24 06:02:34 +0000

    cd pipeline test

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -31,7 +31,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hidden-dawn-21749.herokuapp.com', 'www.hidden-dawn-21749.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hidden-dawn-21749.herokuapp.com', 'www.hidden-dawn-21749.herokuapp.com',]
 
 
 # Application definition
