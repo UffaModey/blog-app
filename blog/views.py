0c455be1869fc46c63b98151a5207796db471a09
Fafa Modey <fafa.modey@hedgehoglab.com>
2022-01-28 14:43:20 +0000

    accept only valid emails

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import client
 from .models import Emails
 
+# for regular expressions
+import re
+
+# for validating an Email
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+
 def home(request):
     posts = client.entries()
 
@@ -9,13 +16,19 @@ def home(request):
 
         email = request.POST.get('email', None)
 
-        #put email in the database
-        email_object = Emails.objects.create(email=email)
-        email_object.save()
+        # check if the email is valid
+        if (re.fullmatch(regex, email)):
 
-        subscribe_success = "Yay! You have been subscribed"
+            #put email in the database
+            email_object = Emails.objects.create(email=email)
+            email_object.save()
 
-        return render(request, "home.html", {'posts': posts, 'subscribe_success': subscribe_success})
+            subscribe_success = "Yay! You have been subscribed"
+            return render(request, "home.html", {'posts': posts, 'subscribe_success': subscribe_success})
+
+        else:
+            invalid_email = "Enter a valid Email address please :)"
+            return render(request, "home.html", {'posts': posts, 'invalid_email': invalid_email})
 
     else:
         return render(request, "home.html", {'posts': posts})
@@ -39,12 +52,18 @@ def newsletter(request):
     if request.method == 'POST':
         email = request.POST.get('emailn', None)
 
-        #put email in the database
-        email_object = Emails.objects.create(email=email)
-        email_object.save()
+        # check if the email is valid
+        if (re.fullmatch(regex, email)):
+            #put email in the database
+            email_object = Emails.objects.create(email=email)
+            email_object.save()
 
-        subscribe_success = "Yay! You have been subscribed"
-        return render(request, "newsletter.html", {'subscribe_success': subscribe_success})
+            subscribe_success = "Yay! You have been subscribed"
+            return render(request, "newsletter.html", {'subscribe_success': subscribe_success})
+
+        else:
+            invalid_email = "Enter a valid Email address please :)"
+            return render(request, "newsletter.html", {'invalid_email': invalid_email})
 
     else:
         return render(request, "newsletter.html")
