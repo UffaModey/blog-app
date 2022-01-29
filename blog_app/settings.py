a0e9c7a8360d77934eff395cc0c4038e43534c77
Fafa Modey <fafa.modey@hedgehoglab.com>
2022-01-29 06:29:37 +0000

    debug true

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -35,7 +35,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hidden-dawn-21749.herokuapp.com', 'www.hidden-dawn-21749.herokuapp.com',]
 
